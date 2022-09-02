3145de3f18638815a5de7eac7ec70e24ba646560
eng Abubakr Mahdi <engabubakrmahdi@hotmail.com>
2022-09-02 09:38:35 +0200

    Add project files.

---
+++ PARAFileSystemService/PARAService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace PARAFileSystemService
+{
+    public class ParaService
+    {
+
+        private readonly Timer _timer;
+
+        public ParaService()
+        {
+            _timer = new Timer(1000) { AutoReset = true  };
+            _timer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            //TODO: Wirte PARA Method.
+        }
+
+        public void Start()
+        {
+            _timer.Start(); 
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+
+    }
+}
---
+++ PARAFileSystemService/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace PARAFileSystemService
+{
+    class Program
+    {
+        static void Main()
+        {
+        
+        }
+
+        static void InitializeParaService()
+        {
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<ParaService>(s =>
+                {
+                    s.ConstructUsing(paraSerice => new ParaService());
+                    s.WhenStarted(paraService => paraService.Start());
+                    s.WhenStopped(paraService => paraService.Stop());
+                });
+
+                x.RunAsLocalSystem();
+                x.SetServiceName("PARAService");
+                x.SetDisplayName("PARA Service");
+                x.SetDescription("This service used PARA method to organize folders and files.");
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+
+    }
+}
